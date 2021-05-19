64af7a9bfc686dc94e7f54616ad43af59053b8d3
NilminiDharmakeerthi94 <nilmini.dharmakeerthi@gmail.com>
2021-05-19 19:31:15 +0530

    Create helloWorld.js
    'Hello world' example in React.js

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+const element = <h1> hello world </h1>
+console.log(element);
+ReactDOM.render(element, document.getElementById('root'));
