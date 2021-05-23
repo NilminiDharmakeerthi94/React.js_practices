6c006e528b1896bac7f67ab7682e61f3dd6fbd81
NilminiDharmakeerthi94 <nilmini.dharmakeerthi@gmail.com>
2021-05-23 21:10:55 +0530

    Create App.js
    Print the simple text by using react function.

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+import './index.css';
+
+
+
+function App() {
+return <h1> hello </h1> 
+const element = <App name="hhh" />
+
+ReactDOM.render(
+ element,
+  document.getElementById('root')
+  
+);
+
+}
+export default App;
